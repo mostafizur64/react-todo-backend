09ea0642808d6fb41706d6b2feacc38df8bf8817
tarafdar16 <imrantarafdar16@gmail.com>
2024-03-24 13:16:03 +0600

    done

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const app = express();
 require("dotenv").config();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
+const bodyParser = require("body-parser");
 
 // middleware
 // middleware
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.json());
+
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bnfwhg6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
